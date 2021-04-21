7c19d971132381110b4fedae6af1da6a875b3773
Reda <reda.h@outlook.com>
2021-04-21 11:04:26 +0200

    formulaire

---
+++ ionic-first-app/src/app/form/form.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.page.html',
+  styleUrls: ['./form.page.scss'],
+})
+export class FormPage implements OnInit {
+  public person= {
+    name:null,
+    firstName: null,
+    age: null,
+    job:null,
+    competences: [],
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- ionic-first-app/src/app/home/home.page.ts
+++ ionic-first-app/src/app/home/home.page.ts
@@ -1,5 +1,9 @@
+// importation des composants pour l'affichage et la gestion de l'ecran
+
 import { Component } from '@angular/core';
 
+// decorateur
+//permet d'indiquer le role de l'élément suivant
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +11,30 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  public name = 'Reda';
+  public preferedColor='warning';
+  public isListHidden= true;
+  public countryList=[
+    {name:'france', maincity:'paris', flag:'french-flag.jpg',population: 67},
+    {name:'Irlande', maincity:'Dublin', flag:'irlandeflag.jpg',population:5},
+    {name:'allemagne', maincity:'berlin', flag:'deutchflag.jpg',population:85}
+  ];
+  
   constructor() {}
+  public hideTheList(){
+    this.isListHidden=true;
+  }
+  
+  public showHideList(){
+    if(this.isListHidden){
+      this.isListHidden= false;
+    }else {
+      this.isListHidden= true;
+      // 2 eme methode
+      // this.isListHidden=!this.isListHidden;
+    }
+    
+  }
+ 
 
 }
